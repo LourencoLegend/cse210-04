98530b7282577dbe73224fa5bfa6cbe0835fc0d9
LourencoLegend <88846755+LourencoLegend@users.noreply.github.com>
2022-07-16 10:01:06 -0600

    main file update

--- greed/__main__.py
+++ greed/__main__.py
@@ -1,4 +1,3 @@
-import os
 import random
 
 from game.casting.actor import Actor
@@ -14,34 +13,33 @@ from game.shared.color import Color
 from game.shared.point import Point
 
 
-FRAME_RATE = 12
+FRAME_RATE = 30
 MAX_X = 900
 MAX_Y = 600
-CELL_SIZE = 30
-FONT_SIZE = 30
+CELL_SIZE = 15
+FONT_SIZE = 15
+COLS = 60
 ROWS = 40
 CAPTION = "Greed"
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
-DEFAULT_ARTIFACTS = 40
-
-
+DEFAULT_ROCKS = 20
+DEFAULT_GEMS = 20
 def main():
     
     # create the cast
-    cast = Cast()
+    cast = Cast(COLS, ROWS, CELL_SIZE)
     
-    # create the banner
-    banner = Actor()
-    banner.set_text("")
-    banner.set_font_size(FONT_SIZE)
-    banner.set_color(WHITE)
-    banner.set_position(Point(CELL_SIZE, 0))
-    cast.add_actor("banners", banner)
+    # create the score
+    score = Actor()
+    score.set_text("Score: 0 ")
+    score.set_font_size(FONT_SIZE)
+    score.set_color(WHITE)
+    score.set_position(Point(CELL_SIZE, 0))
+    cast.add_actor("scores", score)
     
     # create the robot
     x = int(MAX_X / 2)
-    y = int(MAX_Y - CELL_SIZE)
+    y = int(MAX_Y / 2)
     position = Point(x, y)
 
     robot = Actor()
@@ -51,19 +49,51 @@ def main():
     robot.set_position(position)
     cast.add_actor("robots", robot)
     
-    # create the artifacts
-    for n in range(DEFAULT_ARTIFACTS):
-        artifact = Artifact.random_artifact()
-        cast.add_actor("artifacts", artifact)
-        # to make the artifact move (junhee)
-        # artifact.set_velocity(Point(15,15))
+    # create the rocks
+    for n in range(DEFAULT_ROCKS):        
+        text = str('O')
+        x = random.randint(1, COLS - 1)
+        y = random.randint(1, ROWS - 1)
+        position = Point(x, y)
+        position = position.scale(CELL_SIZE)
 
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = Color(r, g, b)
+        
+        artifact = Artifact()
+        artifact.set_text(text)
+        artifact.set_font_size(FONT_SIZE)
+        artifact.set_color(color)
+        artifact.set_position(position)                         
+        cast.add_actor("rocks", artifact)
+    
+    # create the gems
+    for n in range(DEFAULT_GEMS):        
+        text = str('*')
+        x = random.randint(1, COLS - 1)
+        y = random.randint(1, ROWS - 1)
+        position = Point(x, y)
+        position = position.scale(CELL_SIZE)
+
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = Color(r, g, b)
+        
+        artifact = Artifact()
+        artifact.set_text(text)
+        artifact.set_font_size(FONT_SIZE)
+        artifact.set_color(color)
+        artifact.set_position(position)
+        cast.add_actor("gems", artifact)
+    
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
     video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
     director = Director(keyboard_service, video_service)
     director.start_game(cast)
-    
 
 
 if __name__ == "__main__":
